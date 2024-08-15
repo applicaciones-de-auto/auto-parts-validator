36f56a50e847b33c9c91248f8b467fbad46a8033
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-08-15 17:20:37 +0800

    Added Functionality for Inventory Model and Model Year
    Added Functionality for Inventory Model and Model Year

--- src/main/java/org/guanzon/auto/validator/parts/Validator_Inventory_Model.java
+++ src/main/java/org/guanzon/auto/validator/parts/Validator_Inventory_Model.java
@@ -6,29 +6,65 @@
 package org.guanzon.auto.validator.parts;
 
 import org.guanzon.appdriver.base.GRider;
+import org.guanzon.auto.model.parts.Model_Inventory_Model;
 
 /**
  *
  * @author Arsiela
  */
 public class Validator_Inventory_Model implements ValidatorInterface {
+    GRider poGRider;
+    String psMessage;
+    
+    Model_Inventory_Model poEntity;
 
     public Validator_Inventory_Model(Object foValue) {
+        poEntity = (Model_Inventory_Model) foValue;
     }
 
     @Override
     public void setGRider(GRider foValue) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        poGRider = foValue;
     }
 
     @Override
     public boolean isEntryOkay() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(poEntity.getStockID()== null) {
+            psMessage = "Stock ID is not set.";
+            return false;
+        } else {
+            if (poEntity.getStockID().isEmpty()){
+                psMessage = "Stock ID is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getEntryNo() == null) {
+            psMessage = "Invalid Vehicle Model Entry No.";
+            return false;
+        } else {
+            if (poEntity.getEntryNo() <= 0){
+                psMessage = "Invalid Vehicle Model  Entry No.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getModelCde()== null) {
+            psMessage = "Vehicle Model Code is not set.";
+            return false;
+        } else {
+            if (poEntity.getModelCde().isEmpty()){
+                psMessage = "Vehicle Model Code is not set.";
+                return false;
+            }
+        }
+        
+        return true;
     }
-
+    
     @Override
     public String getMessage() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return psMessage;
     }
     
 }
--- src/main/java/org/guanzon/auto/validator/parts/Validator_Inventory_Model_Year.java
+++ src/main/java/org/guanzon/auto/validator/parts/Validator_Inventory_Model_Year.java
@@ -6,29 +6,66 @@
 package org.guanzon.auto.validator.parts;
 
 import org.guanzon.appdriver.base.GRider;
+import org.guanzon.auto.model.parts.Model_Inventory_Model;
+import org.guanzon.auto.model.parts.Model_Inventory_Model_Year;
 
 /**
  *
  * @author Arsiela
  */
 public class Validator_Inventory_Model_Year implements ValidatorInterface {
+    GRider poGRider;
+    String psMessage;
+    
+    Model_Inventory_Model_Year poEntity;
 
     public Validator_Inventory_Model_Year(Object foValue) {
+        poEntity = (Model_Inventory_Model_Year) foValue;
     }
 
     @Override
     public void setGRider(GRider foValue) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        poGRider = foValue;
     }
 
     @Override
     public boolean isEntryOkay() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(poEntity.getStockID()== null) {
+            psMessage = "Stock ID is not set.";
+            return false;
+        } else {
+            if (poEntity.getStockID().isEmpty()){
+                psMessage = "Stock ID is not set.";
+                return false;
+            }
+        }
+        
+        if(poEntity.getYearModl()== null) {
+            psMessage = "Invalid Vehicle Model Year.";
+            return false;
+        } else {
+            if (poEntity.getYearModl() <= 0){
+                psMessage = "Invalid Vehicle Model Year";
+                return false;
+            }
+        }
+        
+        if(poEntity.getModelCde()== null) {
+            psMessage = "Vehicle Model Code is not set.";
+            return false;
+        } else {
+            if (poEntity.getModelCde().isEmpty()){
+                psMessage = "Vehicle Model Code is not set.";
+                return false;
+            }
+        }
+        
+        return true;
     }
-
+    
     @Override
     public String getMessage() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return psMessage;
     }
     
 }
